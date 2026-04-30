f3e36af11c9ec87ee343cd76a1954291510a8343
Ganesh shaw <izganesh007@gmail.com>
2026-04-30 12:45:43 +0400

    Refactor Scene component and register root function

--- src/remotion/index.ts
+++ src/remotion/index.ts
@@ -1,4 +1,4 @@
-import {AbsoluteFill, Audio, Img, useCurrentFrame, registerRoot, Composition, staticFile} from 'remotion';
+import { AbsoluteFill, Audio, Img, useCurrentFrame, registerRoot, Composition, staticFile } from 'remotion';
 import React from 'react';
 
 const images = ['image1.jfif', 'image2.jfif', 'image3.jfif', 'image4.jfif', 'image5.jfif'];
@@ -6,23 +6,32 @@ const images = ['image1.jfif', 'image2.jfif', 'image3.jfif', 'image4.jfif', 'ima
 const Scene: React.FC = () => {
   const frame = useCurrentFrame();
   const imageIndex = Math.floor(frame / 150) % images.length;
-  const image = images[imageIndex];
+  const currentImage = images[imageIndex];
 
   return (
-    <AbsoluteFill style={{backgroundColor: '#000080'}}>
-      <Img src={staticFile('assets/' + image)} style={{width: '100%', height: '100%', objectFit: 'cover'}} />
+    <AbsoluteFill style={{ backgroundColor: '#000080' }}>
+      <AbsoluteFill style={{ justifyContent: 'center', alignItems: 'center' }}>
+        <Img 
+          src={staticFile(`assets/${currentImage}`)} 
+          style={{ width: '100%', height: '100%', objectFit: 'cover' }} 
+        />
+      </AbsoluteFill>
       <Audio src={staticFile('voiceover.mp3')} />
     </AbsoluteFill>
   );
 };
 
-registerRoot(() => (
-  <Composition
-    id="DynamicComp"
-    component={Scene}
-    durationInFrames={750}
-    fps={30}
-    width={1920}
-    height={1080}
-  />
-));
+export const RemotionRoot: React.FC = () => {
+  return (
+    <Composition
+      id="DynamicComp"
+      component={Scene}
+      durationInFrames={750}
+      fps={30}
+      width={1920}
+      height={1080}
+    />
+  );
+};
+
+registerRoot(RemotionRoot);
